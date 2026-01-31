ee2c6f0f158ca56d23c36594ad326f41fdc50876
Krish Nachnani <knachnani28@gmail.com>
2026-01-31 13:17:38 -0600

    removed f1 cache data + new csvs

---
+++ Python/main.py
@@ -0,0 +1,9 @@
+import numpy
+import matplotlib
+import pandas as pd
+
+import fastf1
+from fastf1 import plotting
+
+
+fastf1.Cache.enable_cache('data/fastf1_cache')  # Enable caching for faster loading
